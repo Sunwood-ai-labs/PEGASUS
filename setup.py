39fadcda3b29b0128496bb1ffed0429fad9395f7
Maki <sunwood.ai.labs@gmail.com>
2024-06-09 10:42:36 +0900

    ⬆️ [chore] バージョンを 0.2.0 から 0.2.1 に更新

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pegasus-surf',
-    version='0.2.0',
+    version='0.2.1',
     description='A package for scraping websites and converting them to Markdown',
     author='Maki',
     author_email='sunwood.ai.labs@gmail.com',
